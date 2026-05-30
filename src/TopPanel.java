a73adbb399f9e00911e987948b99576fd47f5095
Kainas <165678304+kainaas@users.noreply.github.com>
2026-05-30 10:03:07 -0300

    slightly changes to Icons and TopPanel

--- src/TopPanel.java
+++ src/TopPanel.java
@@ -1,12 +1,16 @@
 import java.awt.BorderLayout;
 import javax.swing.ButtonGroup;
-import javax.swing.ImageIcon;
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
 import javax.swing.JTextArea;
 import javax.swing.JToggleButton;
+
+import javax.swing.ImageIcon;
+import java.awt.Image;
+
+//TODO: create a class with constants indicating the colors and images of light mode and dark mode
 
 public class TopPanel extends JPanel {
     
@@ -21,11 +25,11 @@ public class TopPanel extends JPanel {
     public TopPanel() {
         super();
         //setLayout();
-        createToggleButtons();
+        createButtons();
         addToggleButtons();
     }
 
-    private void createToggleButtons() {
+    private void createButtons() {
         this.dayView = new JToggleButton("Day", true); //starts viewing day
         this.weekView = new JToggleButton("Week", false);
         this.monthView = new JToggleButton("Month", false);
@@ -37,15 +41,30 @@ public class TopPanel extends JPanel {
         this.chooseView.add(monthView);
         this.chooseView.add(yearView);
 
-        this.dark = new ImageIcon("../imgs/hihihiha.jpg");
-        this.light = new ImageIcon("../imgs/hihihiha.jpg");
+        this.dark = new ImageIcon("../imgs/lightMode/darkMode.png"); //TODO: put the right paths.
+        this.light = new ImageIcon("../imgs/lightMode/lightMode.png");
+        Image scaledDark = this.dark.getImage().getScaledInstance(20, 20, Image.SCALE_SMOOTH);
+        Image scaledLight = this.light.getImage().getScaledInstance(20, 20, Image.SCALE_SMOOTH);
+        this.dark = new ImageIcon(scaledDark);
+        this.light = new ImageIcon(scaledLight);
         
         this.darkMode = new JToggleButton(this.dark, true);
         this.lightMode = new JToggleButton(this.light, false);
+        //this.darkMode.setBackground(getBackground());
 
         this.chooseDarkMode = new ButtonGroup();
         this.chooseDarkMode.add(this.darkMode);
         this.chooseDarkMode.add(this.lightMode);
+
+        this.edit = new ImageIcon("../imgs/lightMode/editIcon.png");
+        this.create = new ImageIcon("../imgs/lightMode/createIcon.png");
+        Image scaledEdit = this.edit.getImage().getScaledInstance(20, 20, Image.SCALE_SMOOTH);
+        Image scaledCreate = this.create.getImage().getScaledInstance(20, 20, Image.SCALE_SMOOTH);
+        this.edit = new ImageIcon(scaledEdit);
+        this.create = new ImageIcon(scaledCreate);
+
+        this.createButton = new JButton(edit);
+        this.editButton = new JButton(create);
     }
 
     private void addToggleButtons() {
@@ -55,12 +74,16 @@ public class TopPanel extends JPanel {
         this.add(this.yearView);
         this.add(this.darkMode);
         this.add(this.lightMode);
+        
+        this.add(this.createButton);
+        this.add(this.editButton);
     }
 
-    public static void main(String[] args) {
+   public static void main(String[] args) {
         JFrame frame = new JFrame();
         TopPanel painel = new TopPanel();
         frame.setSize(1920, 1080);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.add(painel, BorderLayout.NORTH);
         frame.setVisible(true);
     }
